550d7353b7927260253c553da22da465793aa068
gshau <gshaughnessy@me.com>
2018-01-17 10:52:55 -0600

    Added db comm

--- app/model.py
+++ app/model.py
@@ -1,8 +1,75 @@
-def ModelIt(fromUser  = 'Default', births = []):
-  in_month = len(births)
-  print('The number born is %i' % in_month)
-  result = in_month
-  if fromUser != 'Default':
-    return result
-  else:
-    return 'check your input'
+
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+import psycopg2
+import pandas as pd
+import numpy as np
+
+dbname = 'ncaabb'
+username = 'gshau' # change this to your username
+engine = create_engine('postgres://%s@localhost/%s'%(username,dbname))
+
+
+def loadTourney(year):
+    df=pd.read_sql('SELECT * FROM tourney_team_'+str(year), engine,index_col='index')
+    return df
+
+def loadTeamData(year):
+    df=pd.read_sql('SELECT * FROM team_data_'+str(year), engine,index_col='index')
+    return df
+
+def getYears():
+    df=pd.read_sql("SELECT table_name FROM information_schema.tables WHERE table_schema='public' AND table_type='BASE TABLE';", engine)
+    yearList=sorted(np.unique(np.array([int(tn.split('_')[-1]) if 'team' in tn else 0 for tn in df.table_name.values])),reverse=True)[:-1]
+    return yearList
+
+
+
+class Tournament:
+    def __init__(self,teamList):
+        self.teamList = np.array(teamList)
+        self.nTeams = len(teamList)
+#         self.teamDict =
+
+
+    def getPairings(self,teamList):
+        pairings=[]
+        for iteam in range(len(teamList)/2):
+            pairings.append([teamList[2*iteam],teamList[2*iteam+1]])
+        return pairings
+
+    def setModel(self,model):
+        self.model = model
+
+    def predictGame(self,game):
+        outcome = model(game[0].properties,game[1].properties)
+        return outcome
+
+    def simulate(self,probThr):
+
+        teamList = self.teamList
+        nTeamRemain = len(teamList)
+        nRound = 1
+        teamLists={}
+        teamLists[nRound]=teamList
+        while nTeamRemain > 1:
+            pairs = self.getPairings(teamList)
+            teamList=[]
+            for p in pairs:
+#                 print('Pair: ',p[0].name, p[1].name,nTeamRemain)
+                prob = self.predictGame(p)
+                if prob > probThr():
+                    teamList.append(p[0])
+                else:
+                    teamList.append(p[1])
+            nRound+=1
+            teamLists[nRound]=teamList
+            nTeamRemain = len(teamList)
+
+        return teamLists
+
+
+class Team:
+    def __init__(self,name,properties):
+        self.name=name
+        self.properties=properties
--- app/ncaatourney/views.py
+++ app/ncaatourney/views.py
@@ -5,70 +5,37 @@ from sqlalchemy_utils import database_exists, create_database
 import pandas as pd
 import psycopg2
 
+import model
+
 # from a_Model import ModelIt
 
 user='gshau'
 host='localhost'
 dbname='birth_db'
 
-db = create_engine('postgres://%s%s/%s'%(user,host,dbname))
-con = None
-con = psycopg2.connect(database = dbname, user = user)
+dbname = 'ncaabb'
+username = 'gshau' # change this to your username
+engine = create_engine('postgres://%s@localhost/%s'%(username,dbname))
 
-viewData={}
-nTeams = 32
-viewData['nTeams']=nTeams
-viewData['teamNames']=["Team "+str(i+1) for i in range(nTeams)]
+year='2017'
+years = model.getYears()
+
+def fetchTeam(year):
+    df=model.loadTourney(year)
+
+    viewData={}
+    regions=df.regionName.unique()
+    viewData['regionList']=[regions[iregion] for iregion in [0, 3, 1, 2]]
+    viewData['teamNames']=[[tname for tname in df[df.regionName==regions[iregion]].teamName.values] for iregion in [0, 3, 1, 2]]
+    viewData['seeds']=[seed for seed in df[df.regionName==regions[0]].seed.values]
+    viewData['nTeams']=len(viewData['teamNames'])
+    return viewData
 
 @app.route('/')
 @app.route('/index')
+@app.route('/year/<int:year>')
+def index(year=2017):
 
-def index():
+    viewData = fetchTeam(year)
     return render_template("bracket_template.html",
-       data = viewData)
-
-
-
-#
-# @app.route('/db')
-# def birth_page():
-#     sql_query = """
-#                 SELECT * FROM birth_data_table WHERE delivery_method='Cesarean';
-#                 """
-#     query_results = pd.read_sql_query(sql_query,con)
-#     births = ""
-#     for i in range(0,10):
-#         births += query_results.iloc[i]['birth_month']
-#         births += "<br>"
-#     return births
-#
-# @app.route('/db_fancy')
-# def cesareans_page_fancy():
-#     sql_query = """
-#                SELECT index, attendant, birth_month FROM birth_data_table WHERE delivery_method='Cesarean';
-#                 """
-#     query_results=pd.read_sql_query(sql_query,con)
-#     births = []
-#     for i in range(0,query_results.shape[0]):
-#         births.append(dict(index=query_results.iloc[i]['index'], attendant=query_results.iloc[i]['attendant'], birth_month=query_results.iloc[i]['birth_month']))
-#     return render_template('cesareans.html',births=births)
-#
-#
-# @app.route('/input')
-# def cesareans_input():
-#     return render_template("input.html")
-#
-# @app.route('/output')
-# def cesareans_output():
-#   #pull 'birth_month' from input field and store it
-#   patient = request.args.get('birth_month')
-#     #just select the Cesareans  from the birth dtabase for the month that the user inputs
-#   query = "SELECT index, attendant, birth_month FROM birth_data_table WHERE delivery_method='Cesarean' AND birth_month='%s'" % patient
-#   print(query)
-#   query_results=pd.read_sql_query(query,con)
-#   print(query_results)
-#   births = []
-#   for i in range(0,query_results.shape[0]):
-#       births.append(dict(index=query_results.iloc[i]['index'], attendant=query_results.iloc[i]['attendant'], birth_month=query_results.iloc[i]['birth_month']))
-#       the_result = ModelIt(patient,births)
-#   return render_template("output.html", births = births, the_result = the_result)
+       data = viewData, year = year, years = years)
