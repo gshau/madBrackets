b6154c174ef86394cb602c887ce7cf6e07eb975c
gshau <gshaughnessy@me.com>
2018-01-28 15:31:36 -0600

    analysis improvements and interface tweaks

--- app/ncaatourney/views.py
+++ app/ncaatourney/views.py
@@ -10,14 +10,13 @@ import model
 
 user='gshau'
 host='localhost'
-dbname='birth_db'
-
 dbname = 'ncaabb'
 username = 'gshau' # change this to your username
+
 engine = create_engine('postgres://%s@localhost/%s'%(username,dbname))
 
 years = model.getYears()
-# years=[2017,2016]
+
 year=2017
 pointsByRound=[1,2,4,8,16,32]
 poolSizes=np.arange(5,101,5)
@@ -32,23 +31,20 @@ def index(year=2017):
         initialBracket={1: bracket[1], 2: ['']*32, 3: ['']*16, 4: ['']*8, 5: ['']*4, 6: ['']*2, 7: ['']*1}
         risk = 0.4
         poolSize=25
-        # print(bracket)
-        return render_template("bracket.html",risk=risk,
-       poolSizes=poolSizes,poolSize=poolSize,
-       newBracket=initialBracket,
-       bracket=initialBracket,
-       firstBracket=initialBracket,
-       bracketOutcome=tourney.bracketOutcome,
-       year = year, years = years,
-       payout=[0,0,0,0],
-       ptile0=[0,0,0,0],
-       ptile1=[0,0,0,0],
-       bustPCT = '1')
-           # bracket=initialBracket, year = year, years = years)
-
-        # return render_template("bracket_template.html",
-           # bracket=initialBracket, year = year, years = years)
-
+        return render_template("bracket.html",
+           risk=risk,
+           poolSizes=poolSizes,
+           poolSize=poolSize,
+           newBracket=initialBracket,
+           firstBracket=initialBracket,
+           secondBracket=initialBracket,
+           bracketOutcome=tourney.bracketOutcome,
+           year = year, years = years,
+           payout=[0,0,0,0],
+           ptile0=[0,0,0,0],
+           ptile1=[0,0,0,0],
+           bustPCT = '1',
+           points=[0,0])
 
 @app.route('/actualBracket/<int:year>')
 def actualBracket(year=2017):
@@ -56,18 +52,9 @@ def actualBracket(year=2017):
     return render_template("bracket_template.html",
        bracket=tourney.bracketOutcome, year = year, years = years)
 
-#
-# @app.route('/populate/<int:year>')
-# def populate(year=2017):
-#     tourney = model.Tournament(year=year)
-#     bracket=tourney.simulate()
-#     return render_template("bracket_template.html",
-#        bracket=bracket, year = year, years = years)
 
-# @app.route('/generate/<int:year>')
 @app.route('/')
 @app.route('/year/<int:year>')
-# @app.route('/year/<int:year>/actual/1')
 def generate(year=2017,risk=0.3,poolSize=25):
     tourney = model.Tournament(year=year)
     bracket=tourney.simulate()
@@ -76,32 +63,34 @@ def generate(year=2017,risk=0.3,poolSize=25):
     pool.loadTourneySets()
 
     pool.getGoodBracket(verbose=True)
-
-    newBracket = pool.summary['newBracket']
+    pointBackTest1=model.compareBrackets(pool.summary['favBracket'],pool.tourney.bracketOutcome,pool.pointSystem)
+    pointBackTest2=model.compareBrackets(pool.summary['newBracket'],pool.tourney.bracketOutcome,pool.pointSystem)
 
     return render_template("bracket.html",risk=risk,
        poolSizes=poolSizes,poolSize=poolSize,
        newBracket=pool.summary['newBracket'],
-       bracket=pool.summary['newBracket'],
        firstBracket=pool.summary['favBracket'],
-       bracketOutcome=pool.tourney.bracketOutcome,
+       secondBracket=pool.summary['newBracket'],
+       # bracketOutcome={},
+       bracketOutcome = pool.tourney.bracketOutcome,
        year = year, years = years,
        payout=list(pool.summary['expPayout'].tolist()),
        ptile0=list(pool.summary['expPlace'][:,0].tolist()),
        ptile1=list(pool.summary['expPlace'][:,1].tolist()),
-       bustPCT = pool.summary['bustPCT'])
+       bustPCT = pool.summary['bustPCT'],
+       points=[pointBackTest1,pointBackTest2])
 
 
 @app.route('/set_parms', methods=['GET', 'POST'])
 def setParms():
     data = request.args
     # print(data)
-    riskLevel = float(data['risk'])*0.2
+    riskLevel = float(data['risk'])#0.4
+    print('RL: ',riskLevel)
     year = int(data['year'])
     poolSize = int(data['poolSize'])
     if poolSize > poolSizes.max():
         poolSize = poolSizes.max()
-    # print(float(riskLevel)*0.1)
     return generate(year,risk=riskLevel,poolSize=poolSize)
 
 
@@ -109,9 +98,9 @@ def setParms():
 def contact():
     return render_template("contact.html")
 
-@app.route('/pointSystem')
-def pointSystem():
-    return render_template("point_system.html")
+# @app.route('/pointSystem')
+# def pointSystem():
+#     return render_template("point_system.html")
 
 @app.errorhandler(404)
 def page_not_found(error):
